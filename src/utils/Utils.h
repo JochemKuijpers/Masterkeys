219a82a84e0c051931e02475c9c20864fc62bc31
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2018-05-06 15:55:26 +0200

    Cast values before clamp comparison

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -3,8 +3,8 @@
 
 template<typename T, typename U, typename V, typename W>
 inline T clamp(U min, V val, W max) {
-    if (val < min) { return (T) min; }
-    if (val > max) { return (T) max; }
+    if ((T)val < (T)min) { return (T)min; }
+    if ((T)val > (T)max) { return (T)max; }
     return (T)val;
 };
 
